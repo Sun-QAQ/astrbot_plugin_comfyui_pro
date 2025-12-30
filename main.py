d2fe4e3d8e4d472c3fdd58683e4ff1c8fd473bca
lumingya <lum204647@gmail.com>
2025-12-30 08:06:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ PLUGIN_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
     "astrbot_plugin_comfyui_pro",  
     "lumingya",                    
     "ComfyUI Pro 连接器",           
-    "1.3.0",
+    "2.2",
     "https://github.com/lumingya/astrbot_plugin_comfyui_pro" 
 )
 class ComfyUIPlugin(Star):
